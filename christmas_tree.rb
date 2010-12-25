59f55705671f33dd56ae582e54e4e0a65efc8f8b
Camille Roux <contact@camilleroux.com>
2010-12-25 19:07:30 +0100

    *Add color *Nicer shape

--- christmas_tree.rb
+++ christmas_tree.rb
@@ -1,1 +1,1 @@
-((1..20).to_a+[6]*4).each{|i|puts ('#'*i*2).center(80)};puts;puts 'Merry Christmas'.center(80)
+puts "\e[32m";((1..6).to_a+(3..9).to_a+(6..12).to_a+[2]*4).each{|i|puts ("#"*i*4).center(80)};puts "\e[31m\n\n"+"Merry Christmas".center(80)
